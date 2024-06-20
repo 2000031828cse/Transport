1772785322d5f9c35a65baf939a3a0dbabe4c292
2000031828cse <121143584+2000031828cse@users.noreply.github.com>
2024-06-20 16:54:47 +0530

    removed Page Header in Transactions folder

--- src/content/applications/Transactions/index.tsx
+++ src/content/applications/Transactions/index.tsx
@@ -1,7 +1,37 @@
+// import { Helmet } from 'react-helmet-async';
+// import PageTitleWrapper from 'src/components/PageTitleWrapper';
+// import { Grid, Container } from '@mui/material';
+
+// import RecentOrders from './RecentOrders';
+
+// function ApplicationsTransactions() {
+//   return (
+//     <>
+//       <Helmet>
+//         <title>Transactions - Applications</title>
+//       </Helmet>
+//       <Container maxWidth="lg">
+//         <Grid
+//           container
+//           direction="row"
+//           justifyContent="center"
+//           alignItems="stretch"
+//           spacing={3}
+//         >
+//           <Grid item xs={12}>
+//             <RecentOrders />
+//           </Grid>
+//         </Grid>
+//       </Container>
+//     </>
+//   );
+// }
+
+// export default ApplicationsTransactions;
+
 import { Helmet } from 'react-helmet-async';
-import PageHeader from './PageHeader';
 import PageTitleWrapper from 'src/components/PageTitleWrapper';
-import { Grid, Container } from '@mui/material';
+import { Grid, Container, Box } from '@mui/material';
 
 import RecentOrders from './RecentOrders';
 
@@ -11,9 +41,6 @@ function ApplicationsTransactions() {
       <Helmet>
         <title>Transactions - Applications</title>
       </Helmet>
-      <PageTitleWrapper>
-        <PageHeader />
-      </PageTitleWrapper>
       <Container maxWidth="lg">
         <Grid
           container
@@ -23,6 +50,7 @@ function ApplicationsTransactions() {
           spacing={3}
         >
           <Grid item xs={12}>
+            <Box mb={4}> </Box>
             <RecentOrders />
           </Grid>
         </Grid>
