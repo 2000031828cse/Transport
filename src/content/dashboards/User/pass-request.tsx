048fef66a7e973be8e1a34878aa9b0659712ff7e
Sujeeth Kolli <kolli.sujeeth@gmail.com>
2024-06-21 15:25:34 +0530

    user

---
+++ src/content/dashboards/User/pass-request.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PassRequest: React.FC = () => {
+  return (
+    <div>
+      <h2>Pass Request</h2>
+      <p>Placeholder for pass request content.</p>
+    </div>
+  );
+};
+
+export default PassRequest;
