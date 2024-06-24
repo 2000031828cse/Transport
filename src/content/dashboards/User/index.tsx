cdef46afc97b4357121f52f71f5224d79caeaf4b
Sujeeth Kolli <kolli.sujeeth@gmail.com>
2024-06-24 16:26:06 +0530

    sidebar

--- src/content/dashboards/User/index.tsx
+++ src/content/dashboards/User/index.tsx
@@ -8,48 +8,7 @@ function DashboardUser() {
       <Helmet>
         <title>User Dashboard</title>
       </Helmet>
-      <Container maxWidth="lg">
-        <Grid container spacing={4}>
-          {/* Sidebar */}
-          <Grid item xs={3}>
-            <List>
-              <ListItem disablePadding>
-                <Button
-                  component={RouterLink}
-                  to="/user-dashboard/routes"
-                  fullWidth
-                >
-                  Routes
-                </Button>
-              </ListItem>
-              <ListItem disablePadding>
-                <Button
-                  component={RouterLink}
-                  to="/user-dashboard/pass-details"
-                  fullWidth
-                >
-                  Pass Details
-                </Button>
-              </ListItem>
-              <ListItem disablePadding>
-                <Button
-                  component={RouterLink}
-                  to="/user-dashboard/pass-request"
-                  fullWidth
-                >
-                  Pass Request
-                </Button>
-              </ListItem>
-            </List>
-          </Grid>
-          {/* Main Content Area */}
-          <Grid item xs={9}>
-            {/* Content for each route will be rendered based on the Route component */}
-          </Grid>
-        </Grid>
-      </Container>
     </>
   );
 }
-
 export default DashboardUser;
