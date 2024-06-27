cdfcf449c544655b531d512df9ccaa72354e0bf6
Sujeeth Kolli <kolli.sujeeth@gmail.com>
2024-06-27 11:50:50 +0530

    user

--- src/content/dashboards/User/index.tsx
+++ src/content/dashboards/User/index.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import { Container, Grid, List, ListItem, Button } from '@mui/material';
 import { NavLink as RouterLink } from 'react-router-dom';
+import PassDetails from 'src/content/applications/buspassdetails';
 
 function DashboardUser() {
   return (
@@ -8,6 +9,13 @@ function DashboardUser() {
       <Helmet>
         <title>User Dashboard</title>
       </Helmet>
+      <Container maxWidth="lg">
+        <Grid container spacing={4}>
+          <Grid item xs={12}>
+            <PassDetails/>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
